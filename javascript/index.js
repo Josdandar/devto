2fc3c6b795b74b873b9634400d3b6f28b34efe8f
Marco Castañeda <marco.dcc@gmail.com>
2022-08-03 01:28:23 -0500

    Fifth commit, Create Post working, Nav pills correction and card correction

--- javascript/index.js
+++ javascript/index.js
@@ -12,7 +12,7 @@ const insertPost = (posts, arrKeys, userImg) => {
     let template = '';
     template += `
     <a href="./postDetail.html?postId=${arrKeys[0]}" class="post_detail_id">
-    <div class="ind_card my-2">
+    <div class="ind_card border-card">
         <ul class="list-group list-group-flush ">
         <img src="${posts[0].postImage}" class="card-img-top" "alt="...">
         <li class="list-group-item user-p1" >
@@ -28,7 +28,7 @@ const insertPost = (posts, arrKeys, userImg) => {
             </div>
         </li>
         <li class="list-group-item story-text-p1">
-            <div class=" story-title-p1">${posts[0].postTitle}
+            <div class="fw-bold story-title-p1">${posts[0].postTitle}
             </div>
             <div class="fw-light story-reference-p1">${posts[0].postTags}</div>
         </li>
@@ -47,7 +47,7 @@ const insertPost = (posts, arrKeys, userImg) => {
             </div>
         </li>
     </ul>
-    </div></a>
+    </div></a></div>
     `
     postsMainCards.innerHTML = template;
     // Remove the first element from all the arrays
@@ -59,27 +59,27 @@ const insertPost = (posts, arrKeys, userImg) => {
     for (post in posts) {
         template += `
         <a href="./postDetail.html?postId=${arrKeys[post]}" class="post_detail_id2">
-        <div class="ind_card my-2">
-    <ul class="list-group list-group-flush">
+        <div class="ind_card my-2 border-card">
+        <ul class="list-group list-group-flush">
         <li class="list-group-item user-p1">
-            <!--Individual post-->
-            <div class="d-flex user-card-p1">
-                <div class="flex-shrink-0 user-pic" id="user_info">
-                <img src="${userImg[post].userProfilepic}" alt="" srcset="">
-                </div>
-                <div class="flex-grow-1 ms-3 user-story-text-p1 lh-1">
-                    <span class="story-username-p1"><small>${posts[post].postAuthor}</small></span><br>
-                    <span class="story-postdate-p1"><small>${posts[post].postCreationDate}</small></span>
-                </div>
-            </div>
+        <!--Individual post-->
+        <div class="d-flex user-card-p1">
+        <div class="flex-shrink-0 user-pic" id="user_info">
+        <img src="${userImg[post].userProfilepic}" alt="" srcset="">
+        </div>
+        <div class="flex-grow-1 ms-3 user-story-text-p1 lh-1">
+        <span class="story-username-p1"><small>${posts[post].postAuthor}</small></span><br>
+        <span class="story-postdate-p1"><small>${posts[post].postCreationDate}</small></span>
+        </div>
+        </div>
         </li>
         <li class="list-group-item story-text-p1">
-            <div class=" story-title-p1">${posts[post].postTitle}
-            </div>
-            <div class="fw-light story-reference-p1">${posts[post].postTags}</div>
+        <div class="fw-bold story-title-p1">${posts[post].postTitle}
+        </div>
+        <div class="fw-light story-reference-p1">${posts[post].postTags}</div>
         </li>
         <li class="list-group-item">
-            <div class="card-bottom-p1">
+        <div class="card-bottom-p1">
                 <div class="bottom-left-p1">
                     <img src="/assets/images/heart.png" class="properties-icon-p1" alt="" srcset="">
                     ${posts[post].postLikesCount}
@@ -92,23 +92,28 @@ const insertPost = (posts, arrKeys, userImg) => {
             </div>
         </li>
     </ul>
-</div></a>
-    `
-    };
-
+</div></a> `
     postsMainCards2.innerHTML = template;
+    
+};
+
+
     return;
 };
+
 // Define and store the input value on the 'Search' box
 let input = document.querySelector('.input-text');
 let findInput = document.querySelector('#find-input');
 let searchText = '';
 findInput.addEventListener('input', () =>{ searchText = findInput.value});
 
-//When the search i
+//When the search is done and the 'Enter' key is pressed, the search page is displayed with the results
 let searchClick = document.querySelector('.input-search');
 searchClick.addEventListener('search', () =>{ window.location.href = `/search.html?searchId=${searchText}`});
 
+//When clicking on the 'Create Post' button it will display the createPost page.
+let createPostBtn = document.querySelector('.button-create-post');
+createPostBtn.addEventListener('click', () =>{ window.location.href = `/createPost.html`});
 
 // Fetch the info from the db for both the Posts and the Users data, reverse the arrays and call the insertPost function.
 fetch(urlPosts)
@@ -137,7 +142,7 @@ fetch(urlPosts)
     })
     .catch((error) => {
         console.log(error);
-    })
+    });
 
 
 
