2df633c683d96a7cc4a262c860197d22baaba760
Gio <giovannigonzalezdelgado25@gmail.com>
2022-08-04 16:04:25 -0500

    the function and design of detailPost is finished

--- javascript/postDetailluis.js
+++ javascript/postDetailluis.js
@@ -1,4 +1,4 @@
-let posturl ='https://devto-photoapp-default-rtdb.firebaseio.com/posts/-N8bOAEyt1gDoTKduz1p.json';
+// let posturl ='https://devto-photoapp-default-rtdb.firebaseio.com/posts/-N8bOAEyt1gDoTKduz1p.json';
 let userurl ='https://devto-photoapp-default-rtdb.firebaseio.com/users/-N8RJPww_niZjLYxXLB2.json';
 
 const urlParams = new URLSearchParams(window.location.search);
@@ -12,10 +12,10 @@ let postsLeftAside = document.querySelector('#left-aside');
 
 
     templateleft=`
-    <div class="d-flex flex-row col-8 flex-lg-column left-aside-icon-container  col-lg-auto justify-content-evenly py-2" id="icongroup-container">
+<div class="d-flex flex-row col-8 flex-lg-column left-aside-icon-container  col-lg-auto justify-content-evenly py-2" id="icongroup-container">
 
     <div class=" d-flex flex-lg-column col-2  col-lg-1 justify-content-between" id="icon-counter-container">
-        <div class="icon-container">
+        <div class="icon-container" id="like-heart">
             <input type="checkbox" id="aside-icon-checkboxheart" class="checkboxhider">
             <label for="aside-icon-checkboxheart" class="checkedlabel" id="checkboxheaartlabel">
               <img src="./assets/images/SVG/heart.png" alt="" class="aside-icon-default">
@@ -28,7 +28,7 @@ let postsLeftAside = document.querySelector('#left-aside');
         </div>
     </div>
     <div class=" d-flex flex-lg-column col-2  col-lg-1 justify-content-between" id="icon-counter-container">
-        <div class="icon-container">
+        <div class="icon-container" id="unicorn-click">
             <input type="checkbox" id="aside-icon-checkboxunicorn" class="checkboxhider">
             <label for="aside-icon-checkboxunicorn" class="checkedlabel" id="checkboxheaartlabel">
               <img src="./assets/images/SVG/unicorn.png" alt="" class="aside-icon-default">
@@ -41,7 +41,7 @@ let postsLeftAside = document.querySelector('#left-aside');
         </div>
     </div>
     <div class=" d-flex flex-lg-column col-2 justify-content-between col-lg-1" id="icon-counter-container">
-        <div class="icon-container">
+        <div class="icon-container" id="save-flag">
             <input type="checkbox" id="aside-icon-checkboxsave" class="checkboxhider">
             <label for="aside-icon-checkboxsave" class="checkedlabel" id="checkboxheaartlabel">
               <img src="./assets/images/SVG/save.png" alt="" class="aside-icon-default">
@@ -78,8 +78,6 @@ let postsLeftAside = document.querySelector('#left-aside');
             </div>
         </div>
     </div>
-
-
 </div>
 `
     let tagsArray2 = posts.postTags.join(' ')
@@ -110,58 +108,8 @@ let postsLeftAside = document.querySelector('#left-aside');
     <div class="card-text">
         <p>${posts.postBody}</p>
     </div>
-    <div class="card-happy">
-        <strong>
-        Happy branching :corazón:
-        </strong>
-        <div class="comments-container d-flex align-items-start">
-        
-        <div class="form-comments">
-            <div class="form-comment-area" tabindex="0">
-                <textarea id="comment-content" placeholder="Add to the discussion" class="form-control"
-                    aria-label="Add a comment to the discussion"></textarea>
-                <div class="comment-form-toolbar d-none px-2">
-                    <div class="d-flex form-buttons-toolbar align-items-center">
-                        <button type="button" class="btn btn-upload">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                viewBox="0 0 24 24" role="img" class="crayons-icon">
-                                <path
-                                    d="M20 5H4v14l9.292-9.294a1 1 0 011.414 0L20 15.01V5zM2 3.993A1 1 0 012.992 3h18.016c.548 0 .992.445.992.993v16.014a1 1 0 01-.992.993H2.992A.993.993 0 012 20.007V3.993zM8 11a2 2 0 110-4 2 2 0 010 4z">
-                                </path>
-                            </svg>
-                            <span>Upload image</span>
-                        </button>
-                        <button type="button" class="btn btn-upload">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                viewBox="0 0 24 24" role="img" class="crayons-icon">
-                                <path
-                                    d="M3 18.5V5a3 3 0 013-3h14a1 1 0 011 1v18a1 1 0 01-1 1H6.5A3.5 3.5 0 013 18.5zM19 20v-3H6.5a1.5 1.5 0 100 3H19zM10 4H6a1 1 0 00-1 1v10.337A3.485 3.485 0 016.5 15H19V4h-2v8l-3.5-2-3.5 2V4z">
-                                </path>
-                            </svg>
-                            <span>Templates</span>
-                        </button>
-                    </div>
-                    <a href="#" class="">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24"
-                            height="24" role="img"
-                            aria-labelledby="a719kqxeiwm2ywzwnm985y3gfefx3v8p" class="crayons-icon">
-                            <title id="a719kqxeiwm2ywzwnm985y3gfefx3v8p">Editor guide</title>
-                            <path
-                                d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm0-2a8 8 0 100-16 8 8 0 000 16zM11 7h2v2h-2V7zm0 4h2v6h-2v-6z"
-                                fill="">
-                            </path>
-                        </svg>
-                    </a>
-                </div>
-            </div>
-            <div class="comment-form-buttons mt-3 d-none">
-                <button type="button" class="btn btn-submit btn-save-comment" disabled>Submit</button>
-                <button type="button" class="btn btn-preview" disabled>Preview</button>
-            </div>
-        </div>
-    </div>
-    </div>
-    </div>`;
+</div>
+`;
 
      postsDetailMain.innerHTML = template;
      postsLeftAside.innerHTML = templateleft;
@@ -170,7 +118,7 @@ let AuthorDetailMain = document.querySelector('#user-holder');
     const insertUser = (user) => {
         console.log()
          template = `
-         <div class="card my-3  rounded-3 "  id="right-aside-top-card">
+        <div class="card my-3  rounded-3 "  id="right-aside-top-card">
          <div class="card-header-color rounded-top just" id="right-aside-card">
          -
          </div>
@@ -234,30 +182,38 @@ let AuthorDetailMain = document.querySelector('#user-holder');
          return; }
 
 
-fetch(posturl)
-    .then((res) => {
-        return res.json()
-    })
-    .then((res) => {
-        console.log(res)
-        insertPost(res,);
-    })
-    .catch((error) => {
-        console.log(error)
-    })
-// const getPostById = (postId) =>{
-//     let url = 'https://devto-photoapp-default-rtdb.firebaseio.com/posts/' + postId + '/.json'
-//     fetch( url ,{
-//     method: 'GET'
+// fetch(posturl)
+//     .then((res) => {
+//         return res.json()
 //     })
-//     .then(function(plainText){
-//         let jsonResponsePromise = plainText.json()
-//         return jsonResponsePromise
+//     .then((res) => {
+//         console.log(res)
+//         insertPost(res,);
 //     })
-//     .then((postDetail) =>{
-//         console.log(postDetail)
+//     .catch((error) => {
+//         console.log(error)
 //     })
-// }
+/*
+ 1. crear funcion que reciba el postId
+ 2. construir el url con el postId
+ 3. invocar el metodo fetch para llamar el servidor
+ 4. convertir la respuesta del fetch a json
+ 5. leer el objeto de tipo json con la información del post
+ 6. inyectar el detalle del post obtenido por el Id en el html
+  */
+const getPostById = (postId) =>{
+    let url = 'https://devto-photoapp-default-rtdb.firebaseio.com/posts/' + postId + '/.json'
+    fetch( url ,{
+    method: 'GET'
+    })
+    .then(function(plainText){
+        let jsonResponsePromise = plainText.json()
+        return jsonResponsePromise
+    })
+    .then((postDetail) =>{
+        insertPost(postDetail)
+    })
+}
 
 fetch(userurl)
     .then((res) => {
