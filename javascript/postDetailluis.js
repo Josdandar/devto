b9a0eaa50cfbbf9ea5acf6f1686834889331acbf
Gio <giovannigonzalezdelgado25@gmail.com>
2022-08-04 01:30:20 -0500

    ultimos avances de postDetail

--- javascript/postDetailluis.js
+++ javascript/postDetailluis.js
@@ -1,5 +1,5 @@
-// let posturl ='https://devto-photoapp-default-rtdb.firebaseio.com/posts/-N8RJPxsdXMuHVLLs-30.json';
-// let userurl ='https://devto-photoapp-default-rtdb.firebaseio.com/users/-N8RJPww_niZjLYxXLB2.json';
+let posturl ='https://devto-photoapp-default-rtdb.firebaseio.com/posts/-N8bOAEyt1gDoTKduz1p.json';
+let userurl ='https://devto-photoapp-default-rtdb.firebaseio.com/users/-N8RJPww_niZjLYxXLB2.json';
 
 const urlParams = new URLSearchParams(window.location.search);
 const postId = urlParams.get('postId');
@@ -8,7 +8,7 @@ let postsDetailMain = document.querySelector('#post-holder');
 let postsLeftAside = document.querySelector('#left-aside');
 
 
-    const insertPost = (posts) => {
+    const insertPost = (posts, posts2) => {
 
 
     templateleft=`
@@ -81,8 +81,9 @@ let postsLeftAside = document.querySelector('#left-aside');
 
 
 </div>
-    `
-    let tagsArray = posts.postTags.split(" ");
+`
+    let tagsArray2 = posts.postTags.join(' ')
+    let tagsArray = tagsArray2.split(" ");
     console.log(tagsArray)
     let tagstemplate=``;
     tagsArray.forEach(cv => {
@@ -109,36 +110,56 @@ let postsLeftAside = document.querySelector('#left-aside');
     <div class="card-text">
         <p>${posts.postBody}</p>
     </div>
-    <div class="card mb-img">
-        <div class="card-body">
-          <img src="https://res.cloudinary.com/practicaldev/image/fetch/s--sn4XzJyl--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://pbs.twimg.com/profile_images/839643754126417920/6trsFcTQ_normal.jpg" alt="The Practical Dev profile image">
-          <div class="twitter-access">
-            The Practical Dev
-          </div>
-          <div class="twitter-dev">
-            <a href="https://dev.to/thepracticaldev">
-                @thepracticaldev</a>
-          </div>
-          <div class="twitter-logo">
-            <img src="https://res.cloudinary.com/practicaldev/image/fetch/s--ir1kO05j--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://dev.to/assets/twitter-f95605061196010f91e64806688390eb1a4dbc9e913682e043eb8b1e06ca484f.svg" alt="Twitter-log">
-          </div>
-          <div class="twitter-actions">
-            <a href="https://tiwitter.com/intent/twet?in reply to=762709515133485056">
-                <img src="https://res.cloudinary.com/practicaldev/image/fetch/s--fFnoeFxk--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://dev.to/assets/twitter-reply-action-238fe0a37991706a6880ed13941c3efd6b371e4aefe288fe8e0db85250708bc4.svg" alt="coment">
-            </a>
-            <a href="https://twitter.com/intent/retweet?tweet_id=762709515133485056">
-                <img src="https://res.cloudinary.com/practicaldev/image/fetch/s--k6dcrOn8--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://dev.to/assets/twitter-retweet-action-632c83532a4e7de573c5c08dbb090ee18b348b13e2793175fea914827bc42046.svg" alt="">
-            </a>
-            <a href="https://twitter.com/intent/like?tweet_id=762709515133485056">
-                <img src="https://res.cloudinary.com/practicaldev/image/fetch/s--SRQc9lOp--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://dev.to/assets/twitter-like-action-1ea89f4b87c7d37465b0eb78d51fcb7fe6c03a089805d7ea014ba71365be5171.svg" alt="">
-            </a>
-          </div>
-        </div>
-      </div>
     <div class="card-happy">
         <strong>
         Happy branching :corazón:
         </strong>
+        <div class="comments-container d-flex align-items-start">
+        
+        <div class="form-comments">
+            <div class="form-comment-area" tabindex="0">
+                <textarea id="comment-content" placeholder="Add to the discussion" class="form-control"
+                    aria-label="Add a comment to the discussion"></textarea>
+                <div class="comment-form-toolbar d-none px-2">
+                    <div class="d-flex form-buttons-toolbar align-items-center">
+                        <button type="button" class="btn btn-upload">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                viewBox="0 0 24 24" role="img" class="crayons-icon">
+                                <path
+                                    d="M20 5H4v14l9.292-9.294a1 1 0 011.414 0L20 15.01V5zM2 3.993A1 1 0 012.992 3h18.016c.548 0 .992.445.992.993v16.014a1 1 0 01-.992.993H2.992A.993.993 0 012 20.007V3.993zM8 11a2 2 0 110-4 2 2 0 010 4z">
+                                </path>
+                            </svg>
+                            <span>Upload image</span>
+                        </button>
+                        <button type="button" class="btn btn-upload">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                viewBox="0 0 24 24" role="img" class="crayons-icon">
+                                <path
+                                    d="M3 18.5V5a3 3 0 013-3h14a1 1 0 011 1v18a1 1 0 01-1 1H6.5A3.5 3.5 0 013 18.5zM19 20v-3H6.5a1.5 1.5 0 100 3H19zM10 4H6a1 1 0 00-1 1v10.337A3.485 3.485 0 016.5 15H19V4h-2v8l-3.5-2-3.5 2V4z">
+                                </path>
+                            </svg>
+                            <span>Templates</span>
+                        </button>
+                    </div>
+                    <a href="#" class="">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24"
+                            height="24" role="img"
+                            aria-labelledby="a719kqxeiwm2ywzwnm985y3gfefx3v8p" class="crayons-icon">
+                            <title id="a719kqxeiwm2ywzwnm985y3gfefx3v8p">Editor guide</title>
+                            <path
+                                d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm0-2a8 8 0 100-16 8 8 0 000 16zM11 7h2v2h-2V7zm0 4h2v6h-2v-6z"
+                                fill="">
+                            </path>
+                        </svg>
+                    </a>
+                </div>
+            </div>
+            <div class="comment-form-buttons mt-3 d-none">
+                <button type="button" class="btn btn-submit btn-save-comment" disabled>Submit</button>
+                <button type="button" class="btn btn-preview" disabled>Preview</button>
+            </div>
+        </div>
+    </div>
     </div>
     </div>`;
 
@@ -213,30 +234,30 @@ let AuthorDetailMain = document.querySelector('#user-holder');
          return; }
 
 
-// fetch(posturl)
-//     .then((res) => {
-//         return res.json()
-//     })
-//     .then((res) => {
-
-//         insertPost(res);
-//     })
-//     .catch((error) => {
-//         console.log(error)
-//     })
-const getPostById = (postId) =>{
-    let url = 'https://devto-photoapp-default-rtdb.firebaseio.com/posts/' + postId + '/.json'
-    fetch( url ,{
-    method: 'GET'
+fetch(posturl)
+    .then((res) => {
+        return res.json()
     })
-    .then(function(plainText){
-        let jsonResponsePromise = plainText.json()
-        return jsonResponsePromise
+    .then((res) => {
+        console.log(res)
+        insertPost(res,);
     })
-    .then((postDetail) =>{
-        console.log(postDetail)
+    .catch((error) => {
+        console.log(error)
     })
-}
+// const getPostById = (postId) =>{
+//     let url = 'https://devto-photoapp-default-rtdb.firebaseio.com/posts/' + postId + '/.json'
+//     fetch( url ,{
+//     method: 'GET'
+//     })
+//     .then(function(plainText){
+//         let jsonResponsePromise = plainText.json()
+//         return jsonResponsePromise
+//     })
+//     .then((postDetail) =>{
+//         console.log(postDetail)
+//     })
+// }
 
 fetch(userurl)
     .then((res) => {
