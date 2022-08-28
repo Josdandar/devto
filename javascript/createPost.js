bfa640d773eaced6472bb195a063883a52961400
luised2301 <luised2301@gmail.com>
2022-08-28 12:58:33 -0500

    create post conected to DB

--- javascript/createPost.js
+++ javascript/createPost.js
@@ -3,36 +3,9 @@ var quill = new Quill('#editor', {
     placeholder:'Write your post content here....\n'
   });
 
-
-
-
-
-  const createPost = (post) => {
-    let hRequest = new XMLHttpRequest()
-
-    hRequest.open("POST",'https://devto-photoapp-default-rtdb.firebaseio.com/posts.json', false);
-    hRequest.send(JSON.stringify(post));
-
-    return 
-}
-
-var input = document.querySelector('input[name=tags]'),
-tagify = new Tagify( input );
-
-const otherPost = {
-    postAuthor: "El Admin",
-    postBody: "",
-    postChangeDate: "",
-    postCreationDate: "",
-    postImage: "",
-    postLikesCount: 10,
-    postSavedCount: 20,
-    postTags: [],
-    postTimeToRead: "",
-    postTitle: "",
-    postUnicornCount: 30,
-}
-
+const token = localStorage.getItem("token") || ""
+const payload = token.split(".")[1]
+const id = JSON.parse(atob(payload)).id
 let urlPostImg = document.querySelector('#url_post_img')
 let saveImgUrl = document.querySelector('#save_img_url')
 let postTitle = document.querySelector('#input_post_title')
@@ -40,33 +13,58 @@ let postBody = document.querySelector('#editor')
 let publishBtn = document.querySelector("#save_btn")
 
 
+  const URL = "http://localhost:8080/posts"
+  var input = document.querySelector('input[name=tags]'),
+tagify = new Tagify( input );
 
+  publishBtn.addEventListener('click', async (event) => {
+    event.preventDefault()
+    postTags=[],
+    JSON.parse((input.value)).forEach(cv => {
+     postTags.push(cv.value)
+      });
 
-saveImgUrl.addEventListener('click', () => {
-  otherPost.postImage = urlPostImg.value
-})
+    const data ={
+    postAuthor :id,
+    postImage : urlPostImg.value,
+    postTitle : postTitle.value,
+    postBody :quill.root.innerHTML,
+    postTimeToRead : Math.ceil(quill.getText().length/1500),
+    postTags:postTags
+  }
 
-publishBtn.addEventListener('click', () => {
-  otherPost.postImage = urlPostImg.value
-  otherPost.postTitle = postTitle.value
-  otherPost.postBody = quill.root.innerHTML;
-  otherPost.postTimeToRead = Math.ceil(quill.getText().length/1500);
-  const d = new Date();
-  d.getTime();
-  otherPost.postCreationDate =d;  
-  otherPost.postTags=[];
-  console.log(input.value)
-  JSON.parse((input.value)).forEach(cv => {
-  otherPost.postTags.push(cv.value)
-  });
+    console.log(data)
+    console.log(data.postTags)
+
+  const response = await fetch(URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  })
+
+  const jsonData = await response.json()
+  console.log("jsonData : ", jsonData)
+
+  // No fue exitoso, no estas autorizado
+  if(!jsonData.success) {
+    alert(jsonData.message)
+  } else {
+    // Navegar
+    window.location.href = "/index.html"
+  }
+
 
   
-  createPost(otherPost)
-
-  window.location.href="/index.html"
-  
-
 })
+
+
+
+
+
+
+
 
 
 
