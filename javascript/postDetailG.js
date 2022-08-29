c73f2327fab41dce4d9f37bac1e886f25d222a3a
luised2301 <luised2301@gmail.com>
2022-08-29 18:55:05 -0500

    post detail picture fixed

--- javascript/postDetailG.js
+++ javascript/postDetailG.js
@@ -19,12 +19,91 @@ const postOwner= false;
 const urlParams = new URLSearchParams(window.location.search);
 const postId = urlParams.get('postId');
 
-//postId= "630aee947f76158574da42f9"
+
 const postURL = `http://localhost:8080/posts/${postId}`
 
 
 
 createPostBtn.addEventListener('click', () => { window.location.href = `/createPost.html` });
+
+
+
+const insertUser = async () => {
+    url=`http://localhost:8080/users/${posts.postAuthorId}`
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+      }
+    })
+    const jsonData = await response.json()
+    const user = jsonData.data
+    console.log("userpic:",user.userProfilePic)
+    template = `
+    <div class="card my-3  rounded-3 "  id="right-aside-top-card">
+     <div class="card-header-color rounded-top just" id="right-aside-card">
+     -
+     </div>
+     <h5 class="card-header py-4">
+      ${user.userNickname}
+     </h5>
+     <img src="${user.userProfilePic}" alt="" class="position-absolute rounded-circle" id="profile-pic">
+         <div class="card-body d-flex flex-column justify-content-center ">
+             <a href="#" class="btn btn-primary my-2 fw-bold fs-5">
+                 Follow
+             </a>
+             <p class="card-text">
+            
+             </p>
+             <button type="button" class="btn btn-outline-secondary">
+                 Visit Profile
+             </button>
+         </div>
+     </div>
+     <div class="card my-3  " id="right-aside-bottom-card">
+     <div class="card-header py-3 fs-5 fw-bold">
+         More From
+         <a class="text-decoration-none fw-bold fs-5" href="http://127.0.0.1:5502/search.html?searchId=${posts.postAuthor}">
+         ${user.userNickname}
+         </a>
+     </div>
+     <ul class="list-group list-group-flush fs-5">
+         <li class="list-group-item py-3" >
+             <a  class="text-decoration-none fs-6"  id="right-aside-card-list"href="">
+                 <div>
+                     CSS Logos: GitHub logo
+                 </div>
+                 <div>
+                     #CSS #beginners #webdev
+                 </div>
+             </a>
+         </li>
+         <li class="list-group-item py-3" >
+             <a class="text-decoration-none fs-6"  id="right-aside-card-list" href="">
+                 <div>
+                     10 Algorithms Every Developer Should Learn
+                 </div>
+                 <div>
+                     #CSS #beginners #webdev
+                 </div>
+             </a>
+         </li>
+         <li class="list-group-item py-3" >
+             <a class="text-decoration-none fs-6 " id="right-aside-card-list" href="">
+                 <div>
+                     Pathfinding with Javascript: The A* Algorithm
+                 </div>
+                 <div>
+                     #CSS #beginners #webdev
+                 </div>
+             </a>
+         </li>
+     </ul>
+ </div>`;
+     AuthorDetailMain.innerHTML = template;
+     return user
+  }
+
+
 
 addEventListener("DOMContentLoaded", async (e) => {
   e.preventDefault()
@@ -41,7 +120,9 @@ addEventListener("DOMContentLoaded", async (e) => {
   if (posts.postAuthorId===userid){
     editOwner="" 
   }
-
+  
+  const user = await insertUser();
+  console.log("usuario:",user)
   templateleft=`
   <div class="d-flex flex-row col-8 flex-lg-column left-aside-icon-container  col-lg-auto justify-content-evenly py-2" id="icongroup-container">
   
@@ -120,14 +201,14 @@ addEventListener("DOMContentLoaded", async (e) => {
       template = `
     <div class="card ">
       <img src="${posts.postImage}" class="img-top " alt="POSTIMAGE" >
-      <div class="link-dev d-flex flex-row px-5" id="post_author_holder">        
+      <div class="link-dev d-flex flex-row px-5 py-2" id="post_author_holder">        
           <div class="flex position-relative image-title">
               <a href="https://dev.to/devteam">
-                  <img class="image-dev" src="https://picsum.photos/40/40" class="radius-default align-middle" width="40" height="40" alt="The DEV Team p>" alt="">
+                  <img class="image-dev" src="${user.userProfilePic}" class="radius-default align-middle" width="40" height="40" alt="" alt="">
               </a>
           </div>
           <div class="profile-name">
-              <a href="https://dev.to/ben" class="name-profile fw-bold ms-3">${posts.postAuthor}
+              <a href="http://127.0.0.1:5502/search.html?searchId=${posts.postAuthor}" class="name-profile fw-bold ms-3">${posts.postAuthor}
               </a>
           </div>
       </div>
@@ -144,72 +225,13 @@ addEventListener("DOMContentLoaded", async (e) => {
   `;
        postsDetailMain.innerHTML = template;
        postsLeftAside.innerHTML = templateleft;
+       
 })
+
 
 //!de aqyu para abajo no esta terminado
 
-    const insertUser = (user) => {
-         template = `
-        <div class="card my-3  rounded-3 "  id="right-aside-top-card">
-         <div class="card-header-color rounded-top just" id="right-aside-card">
-         -
-         </div>
-         <h5 class="card-header py-4">
-        //*  ${user.userNickname}
-         </h5>
-         <img src="${user.userProfilepic}" alt="" class="position-absolute rounded-circle" id="profile-pic">
-             <div class="card-body d-flex flex-column justify-content-center ">
-                 <a href="#" class="btn btn-primary my-2 fw-bold fs-5">
-                     Follow
-                 </a>
-                 <p class="card-text">
-                 ${user.userDescription}
-                 </p>
-                 <button type="button" class="btn btn-outline-secondary">
-                     Visit Profile
-                 </button>
-             </div>
-         </div>
-         <div class="card my-3  " id="right-aside-bottom-card">
-         <div class="card-header py-3 fs-5 fw-bold">
-             More From
-             <a class="text-decoration-none fw-bold fs-5" href="#Strapi">
-             //*${user.userNickname}
-             </a>
-         </div>
-         <ul class="list-group list-group-flush fs-5">
-             <li class="list-group-item py-3" >
-                 <a  class="text-decoration-none fs-6"  id="right-aside-card-list"href="">
-                     <div>
-                         CSS Logos: GitHub logo
-                     </div>
-                     <div>
-                         #CSS #beginners #webdev
-                     </div>
-                 </a>
-             </li>
-             <li class="list-group-item py-3" >
-                 <a class="text-decoration-none fs-6"  id="right-aside-card-list" href="">
-                     <div>
-                         10 Algorithms Every Developer Should Learn
-                     </div>
-                     <div>
-                         #CSS #beginners #webdev
-                     </div>
-                 </a>
-             </li>
-             <li class="list-group-item py-3" >
-                 <a class="text-decoration-none fs-6 " id="right-aside-card-list" href="">
-                     <div>
-                         Pathfinding with Javascript: The A* Algorithm
-                     </div>
-                     <div>
-                         #CSS #beginners #webdev
-                     </div>
-                 </a>
-             </li>
-         </ul>
-     </div>`;
-         AuthorDetailMain.innerHTML = template;
-         return; }
+
+
+
 
